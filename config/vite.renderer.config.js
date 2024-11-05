1d8523b5db13a657f5146a94ccb0e30ce596e2bd
Dane Bulat <dane.bulat@gmail.com>
2024-11-05 18:20:11 +0700

    vite configs + watch scripts

---
+++ config/vite.renderer.config.js
@@ -0,0 +1,54 @@
+// Copyright 2024 @polkadot-live/polkadot-live-app authors & contributors
+// SPDX-License-Identifier: GPL-3.0-only
+
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import svgr from 'vite-plugin-svgr';
+import { join, resolve } from 'path';
+import { chrome } from '../.electron-vendors.cache.json';
+import { external } from '../vite.base.config';
+
+const PACKAGE_ROOT = join(__dirname, '../');
+const PROJECT_ROOT = PACKAGE_ROOT;
+
+/**
+ * @type {import('vite').UserConfig}
+ * @see https://vitejs.dev/config/
+ */
+export default defineConfig({
+  mode: process.env.MODE,
+  root: PACKAGE_ROOT,
+  envDir: PROJECT_ROOT,
+  base: './',
+  server: {
+    fs: {
+      strict: true,
+    },
+  },
+  resolve: {
+    preserveSymlinks: true,
+    alias: [
+      {
+        find: '@app',
+        replacement: resolve(PACKAGE_ROOT, 'src', 'renderer'),
+      },
+      {
+        find: '@',
+        replacement: resolve(PACKAGE_ROOT, 'src'),
+      },
+    ],
+  },
+  build: {
+    sourcemap: true,
+    target: `chrome${chrome}`,
+    outDir: 'dist/renderer',
+    assetsDir: '.',
+    rollupOptions: {
+      external,
+      input: join(PACKAGE_ROOT, 'index.html'),
+    },
+    emptyOutDir: true,
+    reportCompressedSize: false,
+  },
+  plugins: [react(), svgr()],
+});
